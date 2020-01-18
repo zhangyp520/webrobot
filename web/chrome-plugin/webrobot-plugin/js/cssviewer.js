a36616285b5dbbf2e13f0397115d8a575d197768
flyer555 <944811924@qq.com>
2020-01-18 11:39:02 +0800

    no commit message

--- web/chrome-plugin/webrobot-plugin/js/cssviewer.js
+++ web/chrome-plugin/webrobot-plugin/js/cssviewer.js
@@ -994,9 +994,9 @@ function cssViewerRemoveElement(divid)
 
 //获取xpath
 function readXPath(element) {
-	if (element.id !== "") {//判断id属性，如果这个元素有id，则显 示//*[@id="xPath"]  形式内容
-		return '//*[@id=\"' + element.id + '\"]';
-	}
+	//if (element.id !== "") {//判断id属性，如果这个元素有id，则显 示//*[@id="xPath"]  形式内容
+	//	return '//*[@id=\"' + element.id + '\"]';
+	//}
 	//这里需要需要主要字符串转译问题，可参考js 动态生成html时字符串和变量转译（注意引号的作用）
 	if (element == document.body) {//递归到body处，结束递归
 		return '/html/' + element.tagName.toLowerCase();
@@ -1014,7 +1014,32 @@ function readXPath(element) {
 			ix++;
 		}
 	}
-};
+}
+
+//获取xpath
+function readXPath4all(element) {
+	//if (element.id !== "") {//判断id属性，如果这个元素有id，则显 示//*[@id="xPath"]  形式内容
+	//	return '//*[@id=\"' + element.id + '\"]';
+	//}
+	//这里需要需要主要字符串转译问题，可参考js 动态生成html时字符串和变量转译（注意引号的作用）
+	if (element == document.body) {//递归到body处，结束递归
+		return '/html/' + element.tagName.toLowerCase();
+	}
+	var ix = 1,//在nodelist中的位置，且每次点击初始化
+		siblings = element.parentNode.childNodes;//同级的子元素
+
+	for (var i = 0, l = siblings.length; i < l; i++) {
+		var sibling = siblings[i];
+		//如果这个元素是siblings数组中的元素，则执行递归操作
+		if (sibling == element) {
+			return arguments.callee(element.parentNode) + '/' + element.tagName.toLowerCase()  ;
+			//如果不符合，判断是否是element元素，并且是否是相同元素，如果是相同的就开始累加
+		} else if (sibling.nodeType == 1 && sibling.tagName == element.tagName) {
+			ix++;
+		}
+	}
+}
+
 function readSelector(el) {
 	if (!(el instanceof Element))
 		return '';
@@ -1049,11 +1074,11 @@ function readElementCfg(element){
 	var innerText = element.innerText||"";
 	var tagInfo =outerHtml.replace(innerHtml,'');
 	var path = readXPath(element);
+	var path4all = readXPath4all(element);
 	var selector =  readSelector(element);
-	debugger;
 	var cfg = {
 		xpath:path,//xpath
-
+		xpath4all:path4all,
 		domSelector:selector, //元素定位器
 		domTagName:element.tagName,//元素标签名
 		domId:element.id,//元素id
